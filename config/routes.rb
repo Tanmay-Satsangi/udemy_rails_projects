824cbf11fedf0a472b0e0f820f4b2dab6c72f74a
Tanmay Satsangi <tanmay.satsangi34@gmail.com>
2023-08-17 00:09:31 +0530

    88.Create new articles from browser

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show, :index]
+  #show is just like a get method
+  # index is used for  listing features
+  # create is used to insert the entry to the db
+  resources :articles, only: [:show, :index, :new, :create]
 end
