89df6f9217261aaf38bf07cc2b2226902a1edb6f
Tanmay Satsangi <tanmay.satsangi34@gmail.com>
2023-08-17 01:21:33 +0530

    90.save_articles_to_articles_db and add_missing_validation_from_previous_lecture

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,7 +6,7 @@ class ArticlesController < ApplicationController
   end
 
   def index
-    #This article must be instance variable because we have to use in views
+    #This article must be instance variable because we have to other parts of application also.
     @articles = Article.all
   end
 
@@ -14,7 +14,16 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    render plain: params[:article]
+    #Below line says that require the top level key of article and permit title and description from their to be
+    # used to create this article object or article instance variable.
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    #Below render method is used to print the result into the browser
+    # render plain: @article.inspect
+    @article.save
+
+    #Below statement generates the url: http://localhost:3000/articles/29  where '29' is id of last article saved.
+    redirect_to @article #Here @article contain the id of last saved article
+
   end
 
 end
